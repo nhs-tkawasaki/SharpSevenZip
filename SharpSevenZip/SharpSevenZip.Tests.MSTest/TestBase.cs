b7cd42cd2f1aa15600f7b3cdce906eae548710ea
kawasaki.taiga <kawasaki.taiga.r1z@nhs.co.jp>
2025-11-23 12:53:08 +0900

    add MSTest / xUnit

---
+++ SharpSevenZip/SharpSevenZip.Tests.MSTest/TestBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SharpSevenZip.Tests.MSTest;
+
+public abstract class TestBase
+{
+    protected const string OutputDirectory = "output";
+    protected readonly string TemporaryFile = Path.Combine(OutputDirectory, "tmp.7z");
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+        Directory.CreateDirectory(OutputDirectory);
+    }
+
+    [TestCleanup]
+    public void TearDown()
+    {
+        for (var n = 0; n < 10; n++)
+        {
+            try
+            {
+                if (Directory.Exists(OutputDirectory))
+                    Directory.Delete(OutputDirectory, true);
+                break;
+            }
+            catch
+            {
+                Thread.Sleep(20);
+            }
+        }
+    }
+}
